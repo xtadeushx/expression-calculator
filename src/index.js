4e4be813a8decef1781a759ff7dc86f6444d09d0
Oleksandr Cherkasov <alekscherkasov13@gmail.com>
2021-07-30 06:49:14 +0300

    fill: exppression-calculator

--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
-function eval() {
-    // Do not use eval!!!
-    return;
+function foo(str) {
+    // --- Parse a calculation string into an array of numbers and operators
+    let calculation = [],
+        current = '';
+    for (var i = 0, ch; ch = str.charAt(i); i++) {
+        if ('^*/+-'.indexOf(ch) > -1) {
+            if (current == '' && ch == '-') {
+                current = '-';
+            } else {
+                calculation.push(parseFloat(current), ch);
+                current = '';
+            }
+        } else {
+            current += str.charAt(i);
+        }
+    }
+    if (current != '') {
+        calculation.push(parseFloat(current));
+    }
+    return calculation;
 }
 
-function expressionCalculator(expr) {
-    // write your solution here
+function expressionCalculator(calcs) {
+    calc = foo(calcs)
+    // --- Perform a calculation expressed as an array of operators and numbers
+    var ops = [{ '^': (a, b) => Math.pow(a, b) },
+    { '*': (a, b) => a * b, '/': (a, b) => a / b },
+    { '+': (a, b) => a + b, '-': (a, b) => a - b }],
+        newCalc = [],
+        currentOp;
+    for (var i = 0; i < ops.length; i++) {
+        for (var j = 0; j < calc.length; j++) {
+            if (ops[i][calc[j]]) {
+                currentOp = ops[i][calc[j]];
+            } else if (currentOp) {
+                newCalc[newCalc.length - 1] =
+                    currentOp(newCalc[newCalc.length - 1], calc[j]);
+                currentOp = null;
+            } else {
+                newCalc.push(calc[j]);
+            }
+            //console.log(newCalc);
+        }
+        calc = newCalc;
+        newCalc = [];
+    }
+    if (calc.length > 1) {
+        console.log('Error: unable to resolve calculation');
+        return calc;
+    } else {
+        return calc[0];
+    }
 }
-
 module.exports = {
     expressionCalculator
 }
